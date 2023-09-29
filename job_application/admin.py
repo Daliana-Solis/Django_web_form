2f8e2db615b10556eb79bb87a773c43b78bac23d
Daliana-Solis <dsolisso@uci.edu>
2023-09-28 19:40:29 -0500

    Adding features into admin portal

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Form
 
-admin.site.register(Form)
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email") #show on admin homepage
+    search_fields = ("first_name", "last_name", "email") #search bar can search for those fields
+    list_filter = ("date", "occupation") #filter by date or occupation
+    ordering = ("first_name", ) #order by ...
+    readonly_fields = ("occupation") #read only, cannot change that field admin view
+
+
+admin.site.register(Form, FormAdmin)
