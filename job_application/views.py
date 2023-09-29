1b29637460df9720c13fe6bbf318863f61d7b5aa
Daliana-Solis <dsolisso@uci.edu>
2023-09-28 19:04:04 -0500

    Retrieve user information from the form

--- job_application/views.py
+++ job_application/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .forms import AppForm
 
-# Create your views here.
+def index(request):
+    #get user data from the form
+    if request.method == "POST":
+        form = AppForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            date = form.cleaned_data["date"]
+            occupation = form.cleaned_data["occupation"]
+
+    return render(request,"index.html")
